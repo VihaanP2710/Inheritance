ee788e96d06758b1b7f6c1cea70e4ba5300f2708
vihaanvp <vihaanparlikar27@gmail.com>
2025-06-22 15:01:29 +0530

    Added buisness.py and vehicles.py

---
+++ vehicle.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    def __init__(self, name, max_speed, milage):
+        self.name = name
+        self.max_speed = max_speed
+        self.milage = milage
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 180, 12)
+print("Vehicle name: ", School_bus.name)
+print("Maximum Speed: ", School_bus.max_speed)
+print("Milage", School_bus.milage)
